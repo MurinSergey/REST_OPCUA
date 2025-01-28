229841b3149135830382c43edecd711c6ba0a31f
udokan <udokan@udokan.com>
2025-01-28 15:08:05 +0900

    fix name

--- app/src/models/models_type.py
+++ app/src/models/models_type.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel as PyBaseModel
 from sqlalchemy.orm import DeclarativeBase
 
 SqlAlchemyModelType = TypeVar("SqlAlchemyModelType", bound=DeclarativeBase) # Может быть любым подтипом от sqlalchemy.orm.DeclarativeBase
-PyModelType = TypeVar("PyModelType", bound=PyBaseModel) # Может быть любым подтипом от pydantic.BaseModel
+ListModelType = TypeVar("PyModelType", bound=PyBaseModel) # Может быть любым подтипом от pydantic.BaseModel
 
 RequestSchemaType = TypeVar("RequestSchemaType", bound=PyBaseModel)  # Может быть любым подтипом от pydantic.BaseModel
 CreateSchemaType = TypeVar("CreateSchemaType", bound=PyBaseModel)  # Может быть любым подтипом от pydantic.BaseModel
--- app/src/repositories/src/list_repository.py
+++ app/src/repositories/src/list_repository.py
@@ -1,14 +1,14 @@
 from typing import Generic, Type
 from .crud_base_repository import IAbstractCrudRepository
-from ...models.models_type import PyModelType, CreateSchemaType
+from ...models.models_type import ListModelType, CreateSchemaType
 
-class ListRepository(IAbstractCrudRepository, Generic[PyModelType, CreateSchemaType]):
+class ListRepository(IAbstractCrudRepository, Generic[ListModelType, CreateSchemaType]):
 
-    def __init__(self, model: Type[PyModelType], db_list: list[PyModelType]):
+    def __init__(self, model: Type[ListModelType], db_list: list[ListModelType]):
         self._database = db_list
         self.model = model
 
     #Метод для получения всех записей
-    async def get_all(self) -> list[PyModelType]:
+    async def get_all(self) -> list[ListModelType]:
         res = self._database.copy()
         return res
